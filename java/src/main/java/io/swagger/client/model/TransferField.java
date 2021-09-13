b7070fb4915e97e13400e1d34121f7516687a3f3
paul.gardiner <Paul.Gardiner@rci.rogers.com>
2021-09-13 13:26:19 +0000

    Generated code by SwaggerHub

--- java/src/main/java/io/swagger/client/model/TransferField.java
+++ java/src/main/java/io/swagger/client/model/TransferField.java
@@ -25,7 +25,7 @@ import java.io.IOException;
  * TransferField
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-10T22:19:41.489Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-09-13T13:26:19.399Z[GMT]")
 public class TransferField implements AnyOfTransferDataField {
   @SerializedName("keyName")
   private String keyName = null;
